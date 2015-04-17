e54e78b155fc0ca7e18fc0626f2b6eef1ab120b5
shrimp2t <truongsa@TruongSas-iMac.local>
2015-04-17 10:11:20 +0700

    Fix monster widgets, add functions

---
+++ wp-content/themes/london/woocommerce.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template for displaying Shop
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage London
+ * @since London 1.0
+ */
+
+$sidebar = kt_sidebar();
+get_header(); ?>
+    <div class="container">
+        <?php
+        /**
+         * @hooked
+         */
+        do_action( 'theme_before_main' ); ?>
+        <div class="row">
+            <div id="main" class="<?php echo apply_filters('kt_main_class', 'main-class', $sidebar['sidebar']); ?>">
+                <?php
+                woocommerce_content();
+                ?>
+            </div>
+            <?php if($sidebar['sidebar'] != 'full'){ ?>
+                <div class="<?php echo apply_filters('kt_sidebar_class', 'sidebar', $sidebar['sidebar']); ?>">
+                    <?php dynamic_sidebar($sidebar['sidebar_area']); ?>
+                </div><!-- .sidebar -->
+            <?php } ?>
+        </div><!-- .row -->
+        <?php
+        /**
+         * @hooked
+         */
+        do_action( 'theme_after_main' ); ?>
+    </div><!-- .container -->
+<?php get_footer(); ?>
