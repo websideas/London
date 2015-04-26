2d532ca0ccbeb7decce5633eb066164fb32e9596
shrimp2t <truongsa@TruongSas-iMac.local>
2015-04-26 15:44:36 +0700

    sync style css

--- wp-content/plugins/demo-switcher/index.php
+++ wp-content/plugins/demo-switcher/index.php
@@ -20,8 +20,11 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-function ds_scripts(){
+define('DSW_URL', untrailingslashit(  plugins_url( '/', __FILE__ ) ));
+define('DSW_DIR', untrailingslashit( plugin_dir_path(__FILE__ ) )  );
 
+function dsw_scripts(){
+    wp_enqueue_script( 'london-script', DSW_URL . 'demo-switcher.js', array( 'jquery'), null, true );
 }
 
-add_action( 'wp_enqueue_scripts', 'ds_scripts' , 99999999 );
+add_action( 'wp_enqueue_scripts', 'dsw_scripts' , 99999999 );
