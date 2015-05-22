8740f2b1c7bcaa9d66c9d1db20c75f99c927c2ac
shrimp2t <truongsa@TruongSas-iMac.local>
2015-05-22 09:37:17 +0700

    sync

--- wp-content/themes/london/inc/functions-core.php
+++ wp-content/themes/london/inc/functions-core.php
@@ -10,7 +10,7 @@ if ( !defined('ABSPATH')) exit;
  * @see kt_content_width() for template-specific adjustments.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 1170;
+    $content_width = 1170;
 
 
 
@@ -18,61 +18,61 @@ if ( ! isset( $content_width ) )
 add_action( 'after_setup_theme', 'theme_setup' );
 if ( ! function_exists( 'theme_setup' ) ):
 
-function theme_setup() {
-    /**
-     * Editor style.
-     */
-    add_editor_style();
-    
-    /**
-	 * Add default posts and comments RSS feed links to head
-	 */
-	add_theme_support( 'automatic-feed-links' );
-    
-    /**
-	 * Enable support for Post Formats
-	 */
-	//add_theme_support( 'post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat') );
+    function theme_setup() {
+        /**
+         * Editor style.
+         */
+        add_editor_style();
 
-    /*
-    * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-    * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-    
-    /**
-	 * Allow shortcodes in widgets.
-	 *
-	 */
-	add_filter( 'widget_text', 'do_shortcode' );
-    
-    
-    /**
-	 * Enable support for Post Thumbnails
-	 */
-	add_theme_support( 'post-thumbnails' );
-    
-    
-    if (function_exists( 'add_image_size' ) ) {
-        add_image_size( 'recent_posts', 570, 380, true);
-        add_image_size( 'small', 170, 170, true );
-        add_image_size( 'blog-post', 1040, 390, true );
+        /**
+         * Add default posts and comments RSS feed links to head
+         */
+        add_theme_support( 'automatic-feed-links' );
+
+        /**
+         * Enable support for Post Formats
+         */
+        //add_theme_support( 'post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat') );
+
+        /*
+        * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+        * provide it for us.
+         */
+        add_theme_support( 'title-tag' );
+
+        /**
+         * Allow shortcodes in widgets.
+         *
+         */
+        add_filter( 'widget_text', 'do_shortcode' );
+
+
+        /**
+         * Enable support for Post Thumbnails
+         */
+        add_theme_support( 'post-thumbnails' );
+
+
+        if (function_exists( 'add_image_size' ) ) {
+            add_image_size( 'recent_posts', 570, 380, true);
+            add_image_size( 'small', 170, 170, true );
+            add_image_size( 'blog-post', 1040, 390, true );
+        }
+
+        load_theme_textdomain( THEME_LANG, THEME_DIR . '/languages' );
+
+        /**
+         * This theme uses wp_nav_menu() in one location.
+         */
+        register_nav_menus(array(
+            'primary' => __('Main menu', THEME_LANG),
+            'top'	  => __( 'Top Menu', THEME_LANG ),
+            'bottom'	  => __( 'Bottom Menu', THEME_LANG ),
+        ));
+
     }
-    
-    load_theme_textdomain( THEME_LANG, THEME_DIR . '/languages' );
-    
-    /**
-	 * This theme uses wp_nav_menu() in one location.
-	 */
-	register_nav_menus(array(
-        'primary' => __('Main menu', THEME_LANG),
-        'top'	  => __( 'Top Menu', THEME_LANG ),
-        'bottom'	  => __( 'Bottom Menu', THEME_LANG ),
-    ));
-
-}
 endif;
 
 
@@ -83,9 +83,9 @@ endif;
  * @return void
  */
 if ( ! function_exists( 'kt_meta_title' ) ) {
-	function kt_meta_title() {
-	   
-	}
+    function kt_meta_title() {
+
+    }
 }
 
 
@@ -107,9 +107,9 @@ function london_scripts() {
     wp_enqueue_style( 'owl-carousel-theme', THEME_LIBS . 'owl-carousel/owl.theme.css', array());
 
     if( !is_single() ){
-       wp_enqueue_style( 'easyzoom', THEME_CSS . 'easyzoom.css', array());
+        wp_enqueue_style( 'easyzoom', THEME_CSS . 'easyzoom.css', array());
     }
-	// Load our main stylesheet.
+    // Load our main stylesheet.
     wp_enqueue_style( 'london-main', THEME_CSS . 'style.css', array( 'london-style' ), '20141010' );
     if( kt_is_wc()  ){
         if( is_woocommerce() ){
@@ -120,44 +120,44 @@ function london_scripts() {
 
 
     wp_enqueue_style( 'queries', THEME_CSS . 'queries.css', array('london-main'));
-    
-	// Load the Internet Explorer specific stylesheet.
-	wp_enqueue_style( 'london-ie', THEME_CSS . 'ie.css', array( 'london-style' ), '20141010' );
-	wp_style_add_data( 'london-ie', 'conditional', 'lt IE 9' );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    // Load the Internet Explorer specific stylesheet.
+    wp_enqueue_style( 'london-ie', THEME_CSS . 'ie.css', array( 'london-style' ), '20141010' );
+    wp_style_add_data( 'london-ie', 'conditional', 'lt IE 9' );
+
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
     wp_enqueue_script( 'jquery-ui-tabs' );
-    
+
     wp_enqueue_script( 'easing-script', THEME_JS . 'jquery.easing.1.3.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'bootstrap-script', THEME_LIBS . 'bootstrap/js/bootstrap.min.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'mCustomScrollbar-script', THEME_JS . 'jquery.mCustomScrollbar.min.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'mousewheel-script', THEME_JS . 'jquery.mousewheel.min.js', array( 'jquery' ), null, false );
     wp_enqueue_script( 'customSelect-script', THEME_JS . 'jquery.customSelect.min.js', array( 'jquery' ), null, true );
-    wp_enqueue_script( 'ktSticky-script', THEME_JS . 'jquery.kt.sticky.js', array( 'jquery' ), null, true );    
+    wp_enqueue_script( 'ktSticky-script', THEME_JS . 'jquery.kt.sticky.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'owl-carousel', THEME_LIBS . 'owl-carousel/owl.carousel.min.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'superfish-script', THEME_JS . 'jquery.superfish.custom.js', array( 'jquery', 'hoverIntent' ), null, true );
     wp_enqueue_script( 'magnific-popup-script', THEME_JS . 'jquery.magnific-popup.min.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'matchHeightscript', THEME_JS . 'jquery.matchHeight-min.js', array( 'jquery' ), null, true );
+    wp_enqueue_script( 'countdown-script', THEME_JS . 'jquery.countdown.min.js', array( 'jquery' ), null, true );
+    wp_enqueue_script( 'variations-plugin-script', THEME_JS . 'woo-variations-plugin.js', array( 'jquery' ), null, true );
     if( !is_singular() ){
-        wp_enqueue_script( 'countdown-script', THEME_JS . 'jquery.countdown.min.js', array( 'jquery' ), null, true );
-        wp_enqueue_script( 'variations-plugin-script', THEME_JS . 'woo-variations-plugin.js', array( 'jquery' ), null, true );
         wp_enqueue_script( 'easyzoom', THEME_JS . 'easyzoom.js', array( 'jquery' ), null, true );
     }
 
     wp_enqueue_script( 'waypoints', THEME_JS . 'jquery.waypoints.min.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'kt_footer', THEME_JS . 'jquery.kt.footer.js', array( 'jquery' ), null, true );
-    
+
     wp_enqueue_script( 'london-script', THEME_JS . 'functions.js', array( 'jquery', 'wp-mediaelement' ), null, true );
-	
+
     wp_localize_script( 'london-script', 'ajax_frontend', array(
         'ajaxurl' => admin_url( 'admin-ajax.php' ),
         'security' => wp_create_nonce( 'ajax_frontend' ),
         'current_date' => date_i18n('Y-m-d H:i:s')
     ));
 
-    
+
 }
 add_action( 'wp_enqueue_scripts', 'london_scripts' , 69 );
 
@@ -260,27 +260,27 @@ function kt_comments($comment, $args, $depth) {
 
 
 if ( ! function_exists( 'kt_paging_nav' ) ) :
-/**
- * Display navigation to next/previous set of posts when applicable.
- *
- * @since Twenty Fourteen 1.0
- *
- * @global WP_Query   $wp_query   WordPress Query object.
- * @global WP_Rewrite $wp_rewrite WordPress Rewrite object.
- */
-function kt_paging_nav() {
-	global $wp_query, $wp_rewrite;
-    
-    // Don't print empty markup if there's only one page.
-	if ( $wp_query->max_num_pages < 2 ) {
-		return;
-	}
-    
-    echo get_the_posts_pagination( array(
-        'prev_text'          => __( 'Previous', THEME_LANG ),
-        'next_text'          => __( 'Next', THEME_LANG ),
-        'before_page_number' => '',
-    ) );
-    
-}
+    /**
+     * Display navigation to next/previous set of posts when applicable.
+     *
+     * @since Twenty Fourteen 1.0
+     *
+     * @global WP_Query   $wp_query   WordPress Query object.
+     * @global WP_Rewrite $wp_rewrite WordPress Rewrite object.
+     */
+    function kt_paging_nav() {
+        global $wp_query, $wp_rewrite;
+
+        // Don't print empty markup if there's only one page.
+        if ( $wp_query->max_num_pages < 2 ) {
+            return;
+        }
+
+        echo get_the_posts_pagination( array(
+            'prev_text'          => __( 'Previous', THEME_LANG ),
+            'next_text'          => __( 'Next', THEME_LANG ),
+            'before_page_number' => '',
+        ) );
+
+    }
 endif;
