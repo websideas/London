dd3fa013c73236ecb6ccfac049c510a197606591
shrimp2t <truongsa@TruongSas-iMac.local>
2015-05-23 18:32:42 +0700

    Update recommend plugin

--- wp-content/themes/london/framework/data/data-plugins.php
+++ wp-content/themes/london/framework/data/data-plugins.php
@@ -47,6 +47,16 @@ function kt_register_plugins() {
             'force_deactivation'    => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
             'external_url'      => '', // If set, overrides default API URL and points to an external URL
         ),
+        array(
+            'name'          => 'LayerSlider WP', // The plugin name
+            'slug'          => 'LayerSlider', // The plugin slug (typically the folder name)
+            'source'            => THEME_DIR . 'recommend-plugins/LayerSlider.zip', // The plugin source
+            'required'          => true, // If false, the plugin is only 'recommended' instead of required
+            'version'           => '5.3.1', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
+            'force_activation'      => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
+            'force_deactivation'    => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
+            'external_url'      => '', // If set, overrides default API URL and points to an external URL
+        ),
 		array(
             'name'          => 'London Toolkit', // The plugin name
             'slug'          => 'london-toolkit', // The plugin slug (typically the folder name)
